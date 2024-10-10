9e08c81b0bbee8cf5ac964e0ade857ce6143d8c8
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-10-10 10:33:47 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => {
+    renderBooks(data)
+  })
+  .catch(error => console.error());
   
 }
 
@@ -12,6 +22,4 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
